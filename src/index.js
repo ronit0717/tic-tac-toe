349b1f503e48e2673d4a98c513b8ff18a054f8f5
Ronit Chattopadhyay <ronit0717@QMAC0216.local>
2019-10-01 16:38:44 +0530

    Game code completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+/* old code 
 class Square extends React.Component {
   render() {
     return (
@@ -15,13 +16,26 @@ class Square extends React.Component {
     );
   }
 }
+*/
+
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  )
+}
 
 class Board extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      squares : Array(9).fill(null)
+      squares : Array(9).fill(null),
+      xIsNext : true
     };
   }
 
@@ -34,12 +48,29 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice(); //creates a copy of the original array
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState(
+        {
+          squares: squares, 
+          xIsNext : !this.state.xIsNext
+        }
+      );
   }
 
   render() {
-    const status = 'Next player: X';
+
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner '+winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -78,6 +109,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+//Helper function
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
